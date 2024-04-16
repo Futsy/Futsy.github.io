524df6f4688f9d625754b1aa531f5bf13cc9ce53
Floris Velleman <florisvelleman@gmail.com>
2024-04-16 20:27:42 +0200

    Attempt for interval

--- coordstest/script.js
+++ coordstest/script.js
@@ -2,6 +2,13 @@
 function init() {
     const container = document.getElementById('container');
 
+    setInterval(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            container.innerText += '\n getCurrentPosition()' + new Date().toISOString() + ', coords latitude: ' + position.coords.latitude + ', coords longitude: ' + position.coords.longitude;
+        });
+    }, 5000);
+    
+
     const watchID = navigator.geolocation.watchPosition((position) => {
         container.innerText += '\n' + new Date().toISOString() + ', coords latitude: ' + position.coords.latitude + ', coords longitude: ' + position.coords.longitude;
     });
