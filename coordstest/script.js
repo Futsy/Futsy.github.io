71ba457a01653cc8fd8ffc06d0ccb5239529232b
Floris Velleman <florisvelleman@gmail.com>
2024-04-16 20:41:38 +0200

    Explain error

--- coordstest/script.js
+++ coordstest/script.js
@@ -1,15 +1,19 @@
 
+function error(err) {
+    container.innerText += '\n' + `ERROR(${err.code}): ${err.message}`;
+};
+
 function init() {
     const container = document.getElementById('container');
 
     setInterval(() => {
         navigator.geolocation.getCurrentPosition((position) => {
             container.innerText += '\n getCurrentPosition()' + new Date().toISOString() + ', coords latitude: ' + position.coords.latitude + ', coords longitude: ' + position.coords.longitude;
-        });
+        }, error);
     }, 5000);
     
 
     const watchID = navigator.geolocation.watchPosition((position) => {
         container.innerText += '\n' + new Date().toISOString() + ', coords latitude: ' + position.coords.latitude + ', coords longitude: ' + position.coords.longitude;
-    });
+    }, error);
 };
