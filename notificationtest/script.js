14cd6ccf9b09a1bee81f5972189ae17f4cc1578c
Floris Velleman <florisvelleman@gmail.com>
2024-04-16 21:01:36 +0200

    test for notifications

---
+++ notificationtest/script.js
@@ -0,0 +1,23 @@
+function askNotificationPermission() {
+    // Check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support notifications.");
+        return;
+    }
+    Notification.requestPermission().then((permission) => {
+        // set the button to shown or hidden, depending on what the user answers
+        console.log(permission);
+        if (permission === "granted") {
+            setTimeout(() => {
+                const notification = new Notification("To do list");
+            }, 10000);
+            
+        }
+    });
+};
+
+function init() {
+    const clickable = document.getElementById('enable');
+
+    clickable.addEventListener("click", askNotificationPermission);
+};
